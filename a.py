8aea1de4b48763cf1cce4d4b691154fbcf33c33b
kevinwang5658 <kevinwang5658@gmail.com>
2018-06-13 21:24:38 -0400

    let's go to windows

--- a.py
+++ a.py
@@ -1,23 +1,67 @@
-import webbrowser
-from PIL import Image
-import pytesseract
+import os
+import sys
+import subprocess
 
-import requests
-r = requests.post("http://127.0.0.1:5000/", data={'foo':'Screenshot_20180430-155602.png'})
-print(r.text)
+from googleapiclient.discovery import build
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtPrintSupport import *
 
-# image = Image.open("Screenshot_20180430-155602.png")
-# print(pytesseract.image_to_string(image))
-#
-# new = 2
-#
-# tabUrl = "https://www.google.com/#safe=strict&q="
-#
-# term = "hi"
-#
-# br = webbrowser.get("safari")
-# br.open(tabUrl+term,new=new)
-# br.open(tabUrl+"hihi",new=new)
-# br.open(tabUrl+"hihihi", new=new)
+import pyscreenshot as ImageGrab
+
+sudoPass = 'lamborghini'
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignHCenter)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        text_box = QTextEdit()
+        text_box.setText("abc")
+        text_box.setFixedSize(200, 50)
+        text_box.setReadOnly(True)
+        layout.addWidget(text_box)
+        layout.setAlignment(text_box, Qt.AlignHCenter)
+
+
+        button = QPushButton("DO")
+        button.setFixedSize(200, 50)
+        layout.addWidget(button, 1)
+        layout.setAlignment(button, Qt.AlignHCenter)
+
+        self.show()
+
+        command = '%s %s %s %s' % (os.path.abspath('./osx.sh'), 'QuickTime Player', '500', '500')
+        os.system('echo %s|sudo -S %s' % (sudoPass, command))
+
+def search():
+
+    service = build("customsearch", "v1",
+                    developerKey="AIzaSyB2fBI-MiPty_wgETQScuyCN33wSCtJUsg")
+
+    rse = service.cse().list(
+        q='lectures',
+        cx='011673693733911086344:_vsdh-a05s0',
+        exactTerms='U of T'
+    ).execute()
+
+
+    count = rse['queries']['request'][0]['totalResults']
+
+    print(count)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setApplicationName("abc")
+
+    window = MainWindow()
+    app.exec_()
 
 
